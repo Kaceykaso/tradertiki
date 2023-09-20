ce805ed1f94b8b04a728968d4e00980f3024ec3e
Kaso <kaso@Kasos-Air.localdomain>
2023-09-20 15:29:10 -0700

    Moving files, updating CSS, adding imgs, REMOVING JQUERY

---
+++ src/js/main.js
@@ -0,0 +1,59 @@
+
+/**
+ * Variables and things
+ */
+const firstNames = {
+    0: "Beachbum",
+    1: "Driftwood",
+    2: "Hula",
+    3: "Kon",
+    4: "Pacific",
+    5: "Trader",
+    6: "Tiki",
+    7: "Sandcomber",
+    8: "South"
+};
+const lastNames = {
+    0: "Beach",
+    1: "Island",
+    2: "Sands",
+    3: "Tiki",
+    4: "Tai",
+    5: "Tropics"
+};
+var firstName, lastName;
+const spinner = document.getElementById("spinner");
+const firstNameBox = document.getElementById("firstNameBox");
+const lastNameBox = document.getElementById("lastNameBox");
+
+/**
+ * Clicks and clacks
+ */
+spinner.addEventListener('click', () => {
+    spin(firstNameBox, lastNameBox);
+});
+
+/**
+ * spin: executes logic to populate the input fields on click of the spinner button
+ * @param {HTMLInputElement} firstBox - firstNameBox text input from document
+ * @param {HTMLInputElement} lastBox - lastNameBox text input from document
+ */
+function spin(firstBox, lastBox) {
+    // Grab names
+    firstName = firstNames[Math.floor(Math.random() * Object.keys(firstNames).length)];
+    lastName = lastNames[Math.floor(Math.random() * Object.keys(lastNames).length)];
+    // Shove names in boxes
+    firstBox.value = firstName;
+    lastBox.value = lastName;
+}
+
+/**
+ * If document ready, GOOOOOOOOOOOOOOO!
+ */
+function ready(fn) {
+    if (document.readyState!== 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
